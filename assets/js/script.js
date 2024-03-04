fcaeb769d469d1b4be8bc1d5f82e0cc155f5dee2
Chelsea H <crileyprv@gmail.com>
2024-03-04 18:51:50 +0000

    Retrieve elements from the DOM

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Retrieve elements from the DOM
+
+// Buttons
+
+// ---> Start Quiz 
+const startQuizEl = document.querySelector('#btn-go-to-quiz');
+// ---> Answer Question
+const answer_1El = document.querySelector('#answer--1');
+const answer_2El = document.querySelector('#answer--2');
+const answer_3El = document.querySelector('#answer--3');
+const answer_4El = document.querySelector('#answer--4');
+// ---> Next Question
+const nextQuestionEl = document.querySelector('#btn-forward');
+// ---> Reset Game
+const resetGameEl = document.querySelector('#start-again')
