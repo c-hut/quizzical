55178f033f892a0e49dbc491838b9bd9865807cd
Chelsea H <crileyprv@gmail.com>
2024-03-04 18:52:47 +0000

    Retrieve elements from the DOM (for manipulation of user display)

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,4 +12,15 @@ const answer_4El = document.querySelector('#answer--4');
 // ---> Next Question
 const nextQuestionEl = document.querySelector('#btn-forward');
 // ---> Reset Game
-const resetGameEl = document.querySelector('#start-again')
+const resetGameEl = document.querySelector('#start-again');
+
+// Display Manipulation
+const homePageEl = document.querySelector('.welcome-text');
+const quizPageEl = document.querySelector('.quiz-container');
+const resultPageEl = document.querySelector('.result-container')
+const questionNumberEl = document.querySelector('#question-number');
+const questionEl = document.querySelector('#question');
+const answerBtnsEl = [answer_1El, answer_2El, answer_3El, answer_4El];
+const scoreCounterEl = document.querySelector('#running-tally');
+const finalScoreEl = document.querySelector('#final-score');
+const scoreMessageEl =  document.querySelector('#score-message');
